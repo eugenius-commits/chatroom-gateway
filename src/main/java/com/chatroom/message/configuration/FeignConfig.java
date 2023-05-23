edae7588b6d3467001d4d3f4a384716019e919fb
Eugene Zhilin <eugenez@pacificwest.com>
2023-05-23 11:03:42 -0700

    clean up

--- src/main/java/com/chatroom/message/configuration/FeignConfig.java
+++ src/main/java/com/chatroom/message/configuration/FeignConfig.java
@@ -3,7 +3,7 @@ package com.chatroom.message.configuration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
-@EnableFeignClients(basePackages = "com.eugenez.message")
+@EnableFeignClients(basePackages = "com.chatroom.message")
 @Configuration
 public class FeignConfig {
     
